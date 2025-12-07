b76b5da86ee4e888163f561594da2f9eab0603c6
Rodrigo lopes <rodrigoleiro@gmail.com>
2025-12-07 18:30:27 -0300

    feat: implementando pagina about

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,12 +1,50 @@
 import NavBar from "../../components/navBar/navBar";
 import Container from "../../components/secao/secao";
 import Footer from "../../components/footer/footer";
+
+import style from './contato.module.css'
 const Contact = () => {
     return (
         <>
             <NavBar />
             <Container>
-                <h1>Contato</h1>
+                <section className={style.secaoFormaContato}>
+
+                    <h2>Entre em Contato Conosco</h2>
+
+                    <h3>Escolha a opção que melhor se adapta à sua necessidade:</h3>
+
+                    <p>Formulário de Contato: A maneira mais rápida e eficiente de nos enviar sua mensagem.<br />
+                        Preencha os campos e entraremos em contato o mais breve possível.</p>
+
+                    <p>E-mail: Prefere o contato direto? Envie sua mensagem para:</p>
+
+                    clarananda@gmail.com
+
+                    <p>Telefone/WhatsApp: Se precisar de uma resposta imediata ou prefere conversar:</p>
+
+                    (XX) XXXX-XXXX  (XX) XXXXX-XXXX (WhatsApp)
+
+                </section>
+
+                <form className={style.formContato}>
+
+                    <h2>Formulário de Contato</h2>
+                    <input type="text" placeholder="Digite seu Nome" />
+                    <input type="email" placeholder="Digite seu e-mail" />
+                    <input type="text" placeholder="Digite o Assunto" />
+                    <textarea name="" id="" placeholder="Escreva sua mensagem"></textarea>
+                    <button>Enviar</button>
+                </form>
+
+                <section className={style.containerRight}>
+                    <h3>Horário de Atendimento</h3>
+                    <p>Nossa equipe está disponível para atender você nos seguintes horários:</p>
+
+                    <span>Segunda a Sexta: 9h00 às 18h00 (Horário de Brasília)</span>
+                    <span>Sábados, Domingos e Feriados: Atendimento Indisponível</span>
+
+                </section>
 
             </Container>
 
--- src/pages/QuemSomos/About.jsx
+++ src/pages/QuemSomos/About.jsx
@@ -2,12 +2,64 @@ import Container from "../../components/secao/secao";
 import NavBar from "../../components/navBar/navBar";
 import Footer from "../../components/footer/footer";
 
+import style from './quemsomos.module.css'
+
 const About = () => {
     return (
         <>
             <NavBar />
             <Container>
-                <h1>Quem Somos</h1>
+                <section className={style.container}>
+                    <h1 className={style.title}>Quem Somos</h1>
+
+                    <p>
+                        Bem-vindo(a) à Clara nanda! É um prazer compartilhar com você quem somos, o que fazemos e, acima de tudo, por que fazemos.
+                    </p>
+
+                    <section className={style.container}>
+                        <h3>Nossa História e Missão</h3>
+
+                        <p>
+                            A Clara nanda nasceu em 2021 com um objetivo claro de transformar o mercado de consultoria em tecnologia, tornando soluções complexas acessíveis a todos.
+                        </p>
+
+                        <p>
+                            Nossa Missão é: Entregar produtos/serviços de alta qualidade que superem as expectativas dos nossos clientes e contribuam para o seu sucesso e crescimento.
+                        </p>
+
+                    </section>
+
+                    <section className={style.container}>
+
+                        <h3> O Que Nos Move: Nossos Valores</h3>
+
+                        <p>
+                            Guiamos todas as nossas ações e decisões por um conjunto de valores essenciais que definem nossa cultura e a maneira como interagimos com clientes, parceiros e uns com os outros:
+                        </p>
+
+                        <ul>
+                            <li>Inovação: Buscar constantemente novas soluções e aperfeiçoar o que já fazemos.</li>
+                            <li>Integridade: Agir com transparência, ética e honestidade em todas as relações.</li>
+                            <li>Foco no Cliente: Colocar as necessidades e a satisfação do cliente no centro do nosso trabalho.</li>
+                            <li>Excelência: Comprometimento com a mais alta qualidade em nossos produtos e serviços.</li>
+                            <li>Sustentabilidade: Promover práticas que garantam um impacto positivo a longo prazo.</li>
+                        </ul>
+
+                    </section>
+
+                    <section className={style.container}>
+                        <h3>Nossa Visão de Futuro</h3>
+
+                        <p>
+                            Almejamos ser a principal referência no setor de educação, reconhecida pela inovação e pelo impacto positivo que geramos em nossos clientes.
+                        </p>
+                        <p>
+                            Acreditamos que, com paixão, dedicação e o talento da nossa equipe, podemos alcançar essa visão e continuar a evoluir ao lado de nossos clientes.
+                        </p>
+                    </section>
+
+                </section>
+
 
             </Container>
 
